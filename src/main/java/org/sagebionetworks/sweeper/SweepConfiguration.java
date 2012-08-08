edc4eb3558fc7fce0f86327f51f90fc6d7794b09
Geoff Shannon <earthlingzephyr@gmail.com>
2012-08-08 08:55:37 -0700

    Reformat files again.  No code changes.

--- src/main/java/org/sagebionetworks/sweeper/SweepConfiguration.java
+++ src/main/java/org/sagebionetworks/sweeper/SweepConfiguration.java
@@ -6,78 +6,83 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SweepConfiguration {
-  private class NameFilter implements FileFilter {
+	private class NameFilter implements FileFilter {
 
-	final private Pattern pattern;
+		final private Pattern pattern;
 
-	private NameFilter(Pattern pattern) {
-		this.pattern = pattern;
+		private NameFilter(Pattern pattern) {
+			this.pattern = pattern;
+		}
+
+		public boolean accept(File pathname) {
+			if (pathname.isFile()) {
+				Matcher m = pattern.matcher(pathname.getName());
+				return m.matches();
+			}
+			return false;
+		}
+
 	}
 
-    public boolean accept(File pathname) {
-      if (pathname.isFile()) {
-        Matcher m = pattern.matcher(pathname.getName());
-        return m.matches();
-      }
-      return false;
-    }
+	final private String logBaseDir;
+	/**
+	 * The unique portion of the S3 Bucket name that this log configuration
+	 * should be swept to. Note that ".sagebionetworks.org" is appended to it
+	 * automatically.
+	 */
+	final private String s3BucketName;
+	/**
+	 * The "singleton" instance of the NameFilter associated with this
+	 * SweepConfig. It's filtering is based on the logExpression constructor
+	 * parameter.
+	 */
+	final private SweepConfiguration.NameFilter filter;
 
-  }
+	/**
+	 * Constructs a SweepConfiguration object that signifies one set of log
+	 * files (that match logExpression) that are found under logBaseDir to be
+	 * swept into the S3 bucket s3BucketName.
+	 * 
+	 * @param logBaseDir
+	 *            full pathname to the root log directory
+	 * @param logExpression
+	 *            a regular expression that matches all desired logs - see
+	 *            {@link SweepConfiguration#filter}
+	 * @param s3BucketName
+	 *            see {@link SweepConfiguration#s3BucketName}
+	 */
+	public SweepConfiguration(String logBaseDir, String logExpression,
+			String s3BucketName) {
+		this.logBaseDir = logBaseDir;
+		this.s3BucketName = s3BucketName + ".sagebionetworks.org";
+		this.filter = new SweepConfiguration.NameFilter(
+				Pattern.compile(logExpression));
+	}
 
-  final private String logBaseDir;
-  /**
-   * The unique portion of the S3 Bucket name that this
-   * log configuration should be swept to.  Note that
-   * ".sagebionetworks.org" is appended to it automatically.
-   */
-  final private String s3BucketName;
-  /**
-   * The "singleton" instance of the NameFilter associated with
-   * this SweepConfig.  It's filtering is based on the logExpression
-   * constructor parameter.
-   */
-  final private SweepConfiguration.NameFilter filter;
+	public String getLogBaseDir() {
+		return logBaseDir;
+	}
 
-  /**
-   * Constructs a SweepConfiguration object that signifies one set of
-   * log files (that match logExpression) that are found under logBaseDir
-   * to be swept into the S3 bucket s3BucketName.
-   * @param logBaseDir full pathname to the root log directory
-   * @param logExpression a regular expression that matches all desired logs
-   * - see {@link SweepConfiguration#filter}
-   * @param s3BucketName see {@link SweepConfiguration#s3BucketName}
-   */
-  public SweepConfiguration(String logBaseDir, String logExpression,
-      String s3BucketName) {
-    this.logBaseDir = logBaseDir;
-    this.s3BucketName = s3BucketName + ".sagebionetworks.org";
-    this.filter = new SweepConfiguration.NameFilter(Pattern.compile(logExpression));
-  }
+	public String getS3BucketName() {
+		return s3BucketName;
+	}
 
-  public String getLogBaseDir() {
-    return logBaseDir;
-  }
+	public FileFilter getFilter() {
+		return filter;
+	}
 
-  public String getS3BucketName() {
-    return s3BucketName;
-  }
-
-  public FileFilter getFilter() {
-    return filter;
-  }
-
-  /**
-   * The defined transformation from log file name to S3 key.
-   * The important thing here is that some reasonably unique
-   * identifier is part of the key, because Elastic Beanstalk
-   * will make it so that we'll have as many files per-roll interval
-   * as that roll interval had active instances.
-   * @param ec2Id
-   * @param filename
-   * @return
-   */
-  public String fileNameToKey(String ec2Id, String filename) {
-    return String.format("%s-%s", filename, ec2Id);
-  }
+	/**
+	 * The defined transformation from log file name to S3 key. The important
+	 * thing here is that some reasonably unique identifier is part of the key,
+	 * because Elastic Beanstalk will make it so that we'll have as many files
+	 * per-roll interval as that roll interval had active instances.
+	 * 
+	 * @param ec2Id
+	 * @param filename
+	 * @return
+	 */
+	public String fileNameToKey(String ec2Id, String filename) {
+		return String.format("%s-%s", filename, ec2Id);
+	}
 
 }
--- src/main/java/org/sagebionetworks/sweeper/Sweeper.java
+++ src/main/java/org/sagebionetworks/sweeper/Sweeper.java
@@ -9,74 +9,78 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.Bucket;
 
 /**
- * An application for collecting log files from an ec2 instance and
- * collecting them into s3 buckets.  Configurable on what log-file
- * patterns to match, and which s3 bucket they should be swept to.
- *
+ * An application for collecting log files from an ec2 instance and collecting
+ * them into s3 buckets. Configurable on what log-file patterns to match, and
+ * which s3 bucket they should be swept to.
+ * 
  * Can also specify an renaming scheme for movement to s3.
- *
+ * 
  */
-public class Sweeper
-{
-  private final List<SweepConfiguration> configList;
+public class Sweeper {
+	private final List<SweepConfiguration> configList;
 
-  private final String ec2InstanceId;
-  private AmazonS3 client;
+	private final String ec2InstanceId;
+	private AmazonS3 client;
 
-    public Sweeper(AmazonS3 client, String instanceId, List<SweepConfiguration> configList) {
-      this.client = client;
-      this.ec2InstanceId = instanceId;
-    this.configList = configList;
-  }
+	public Sweeper(AmazonS3 client, String instanceId,
+			List<SweepConfiguration> configList) {
+		this.client = client;
+		this.ec2InstanceId = instanceId;
+		this.configList = configList;
+	}
 
-  public AmazonS3 getClient() {
-    return client;
-  }
+	public AmazonS3 getClient() {
+		return client;
+	}
 
-  public void setClient(AmazonS3 client) {
-    this.client = client;
-  }
+	public void setClient(AmazonS3 client) {
+		this.client = client;
+	}
 
-  public void validateConfigurations() {
-  List<String> myBucketNames = new ArrayList<String>();
+	public void validateConfigurations() {
+		List<String> myBucketNames = new ArrayList<String>();
 
-  for (Bucket bucket : client.listBuckets()) {
-      myBucketNames.add(bucket.getName());
-  }
+		for (Bucket bucket : client.listBuckets()) {
+			myBucketNames.add(bucket.getName());
+		}
 
-    for (SweepConfiguration config : configList) {
-      boolean bucketExists = client.doesBucketExist(config.getS3BucketName());
-    boolean isInMyBuckets = myBucketNames.contains(config.getS3BucketName());
-    if (bucketExists && isInMyBuckets) {
-        continue;
-      } else if (bucketExists) {
-        // Could also try to add a unique identifier to the front/back of bucketname
-        // but this requires a lot more work ;)
-        throw new RuntimeException("Bucketname: \""+config.getS3BucketName()+
-            "\" already exists and is not owned by us!");
-      }
-    }
-  }
+		for (SweepConfiguration config : configList) {
+			boolean bucketExists = client.doesBucketExist(config
+					.getS3BucketName());
+			boolean isInMyBuckets = myBucketNames.contains(config
+					.getS3BucketName());
+			if (bucketExists && isInMyBuckets) {
+				continue;
+			} else if (bucketExists) {
+				// Could also try to add a unique identifier to the front/back
+				// of bucketname
+				// but this requires a lot more work ;)
+				throw new RuntimeException("Bucketname: \""
+						+ config.getS3BucketName()
+						+ "\" already exists and is not owned by us!");
+			}
+		}
+	}
 
-  public void sweepAllFiles() {
-    for (SweepConfiguration config : configList) {
-      List<File> files = findFiles(config);
-      sweep(config, files);
-    }
-  }
+	public void sweepAllFiles() {
+		for (SweepConfiguration config : configList) {
+			List<File> files = findFiles(config);
+			sweep(config, files);
+		}
+	}
 
-  public void sweep(SweepConfiguration config, List<File> filesToSweep) {
-    for (File file : filesToSweep) {
-      String key = config.fileNameToKey(ec2InstanceId, file.getName());
-      client.putObject(config.getS3BucketName(), key, file);
-    }
-  }
+	public void sweep(SweepConfiguration config, List<File> filesToSweep) {
+		for (File file : filesToSweep) {
+			String key = config.fileNameToKey(ec2InstanceId, file.getName());
+			client.putObject(config.getS3BucketName(), key, file);
+		}
+	}
 
-  public List<File> findFiles(SweepConfiguration config) {
-    File dir = new File(config.getLogBaseDir());
-    File[] files = dir.listFiles(config.getFilter());
+	public List<File> findFiles(SweepConfiguration config) {
+		File dir = new File(config.getLogBaseDir());
+		File[] files = dir.listFiles(config.getFilter());
 
-    return Arrays.asList(files);
-  }
+		return Arrays.asList(files);
+	}
 
 }
